d3e66bfdf899f1ae9647b6457054e8924152e3f3
Ashwini <salunkheashwini1998@gmail.com>
2023-01-23 20:53:29 +0530

    Validating EmailId

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.RegularExpressions;
+using System.Xml.Linq;
 
 namespace Day24
 {
@@ -6,6 +7,8 @@ namespace Day24
     {
         public static string Reg_pattern = "^[A-Z]{1}[A-Za-z]{2,}$";
         public static string Reg_pattern2 = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public static string Reg_pattern3 = "^(abc)(.[A-Za-z]+)*@(bl).(co)([.][a-z]{2,})?$";
+
 
         public static void Main(string[] args)
         {
@@ -15,8 +18,14 @@ namespace Day24
             Console.WriteLine("Enter your Last name : ");
             string Lastname = Console.ReadLine();
             Console.WriteLine();
+            Console.WriteLine("Enter your Email ID: ");
+            string Email = Console.ReadLine();
+            Console.WriteLine();
+
             bool val1 = Regex.IsMatch(Firstname, Reg_pattern);
             bool val2 = Regex.IsMatch(Lastname, Reg_pattern2);
+            bool val3 = Regex.IsMatch(Email, Reg_pattern3);
+
 
             if (val1)
             {
@@ -24,12 +33,21 @@ namespace Day24
             }
             else
                 Console.WriteLine("pattern is wrong");
+
             if (val2)
             {
                 Console.WriteLine("Pattern is correct for given Last name: {0}", Lastname);
             }
             else
-                Console.WriteLine("pattern is wrong");
+                Console.WriteLine("pattern Last name is wrong");
+
+
+            if (val3)
+            {
+                Console.WriteLine("Pattern is correct for given mail ID : {0}", Email);
+            }
+            else
+                Console.WriteLine("pattern mail ID  is wrong");
         }
     }
 }
