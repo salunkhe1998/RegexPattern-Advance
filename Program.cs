bc557ccdf4da795f18eea31af5689777e0476b20
Ashwini <salunkheashwini1998@gmail.com>
2023-01-23 20:47:16 +0530

    validating Last Name

--- Program.cs
+++ Program.cs
@@ -5,19 +5,32 @@ namespace Day24
     class Program
     {
         public static string Reg_pattern = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public static string Reg_pattern2 = "^[A-Z]{1}[A-Za-z]{2,}$";
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter your First  Name: ");
-            string name = Console.ReadLine();
+            Console.WriteLine("Enter your First name : ");
+            string Firstname = Console.ReadLine();
             Console.WriteLine();
-            bool val = Regex.IsMatch(name, Reg_pattern);
+            Console.WriteLine("Enter your Last name : ");
+            string Lastname = Console.ReadLine();
+            Console.WriteLine();
+            bool val1 = Regex.IsMatch(Firstname, Reg_pattern);
+            bool val2 = Regex.IsMatch(Lastname, Reg_pattern2);
 
-            if (val)
+            if (val1)
             {
-                Console.WriteLine("Pattern is correct for given user name: {0}", name);
+                Console.WriteLine("Pattern is correct for given First name: {0}", Firstname);
+            }
+            else
+                Console.WriteLine("pattern is wrong");
+            if (val2)
+            {
+                Console.WriteLine("Pattern is correct for given Last name: {0}", Lastname);
             }
             else
                 Console.WriteLine("pattern is wrong");
         }
     }
 }
+
