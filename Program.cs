7d606b0ffa521a1378fc290198f8ad404ed82a90
Ashwini <salunkheashwini1998@gmail.com>
2023-01-23 20:34:43 +0530

    Validation First Name

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Day24
+﻿using System.Text.RegularExpressions;
+
+namespace Day24
 {
     class Program
     {
+        public static string Reg_pattern = "^[A-Z]{1}[A-Za-z]{2,}$";
         public static void Main(string[] args)
         {
-            Console.WriteLine("***** Welcome to user registration Program. *****");
+            Console.WriteLine("Enter your First  Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine();
+            bool val = Regex.IsMatch(name, Reg_pattern);
+
+            if (val)
+            {
+                Console.WriteLine("Pattern is correct for given user name: {0}", name);
+            }
+            else
+                Console.WriteLine("pattern is wrong");
         }
     }
-});
+}
