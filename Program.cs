fb2e792c3c52f463ebbdfdfabcad7db36ea82b90
Ashwini <salunkheashwini1998@gmail.com>
2023-01-23 19:50:01 +0530

    method calling

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day24
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("***** Welcome to user registration Program. *****");
+        }
+    }
+});
