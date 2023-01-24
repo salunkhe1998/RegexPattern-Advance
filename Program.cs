68de455f911e8780bc9887dfa755f61397ac5532
Ashwini <salunkheashwini1998@gmail.com>
2023-01-24 19:15:12 +0530

    Valid Mobile Number

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.RegularExpressions;
+﻿
+
+using System.Text.RegularExpressions;
 using System.Xml.Linq;
 
 namespace Day24
@@ -8,6 +10,8 @@ namespace Day24
         public static string Reg_pattern = "^[A-Z]{1}[A-Za-z]{2,}$";
         public static string Reg_pattern2 = "^[A-Z]{1}[A-Za-z]{2,}$";
         public static string Reg_pattern3 = "^(abc)(.[A-Za-z]+)*@(bl).(co)([.][a-z]{2,})?$";
+        public static string Reg_pattern4 = "^[0-9]{2}\\s[0-9]{10}$";
+
 
 
         public static void Main(string[] args)
@@ -21,10 +25,14 @@ namespace Day24
             Console.WriteLine("Enter your Email ID: ");
             string Email = Console.ReadLine();
             Console.WriteLine();
+            Console.WriteLine("Enter your Mobile Number: ");
+            string Mobileno = Console.ReadLine();
+            Console.WriteLine();
 
             bool val1 = Regex.IsMatch(Firstname, Reg_pattern);
             bool val2 = Regex.IsMatch(Lastname, Reg_pattern2);
             bool val3 = Regex.IsMatch(Email, Reg_pattern3);
+            bool val4 = Regex.IsMatch(Mobileno, Reg_pattern4);
 
 
             if (val1)
@@ -48,7 +56,16 @@ namespace Day24
             }
             else
                 Console.WriteLine("pattern mail ID  is wrong");
+
+            if (val4)
+            {
+                Console.WriteLine("Pattern is correct for given mobile number : {0}", Mobileno);
+            }
+            else
+                Console.WriteLine("pattern mobile number  is wrong");
         }
     }
 }
+
+
 
